ac76813a19803718eb92ca23bcaaa2d8e64fc9ec
Natan200-2 <natanboas21@outlook.com>
2024-01-29 15:51:45 -0300

    App criado e consulta a api funcionando

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Search from "./components/SearchBar/SearchBar";
+
 
 function App() {
+
+  const apikey = 'kYfLOAWGq2g5a2QAArgVhnIdqKrfXz9x';
+
+  const [city, setCity] = useState({});
+  const [search, setSearch] = useState('');
+
+  const searchLocationKey = async (event) => {
+    event.preventDefault();
+    
+    try{
+      const response = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apikey}&q=${search}&language=pt-br`);
+
+      if(!response.ok){
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+
+      if(data.length > 0){
+        const key = data[0].Key;
+        searchLocationAtt(key);
+      }
+
+    } catch(err){
+      console.error(`Error fetching city data: ${err}`)
+    }
+
+  }
+
+  const searchLocationAtt = async (key) => {
+    try{
+      const respClima = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${apikey}&language=pt-br`)
+
+      const dataClima = await respClima.json();
+
+        setCity(dataClima[0]);
+        console.log(dataClima[0].WeatherText);
+
+    }catch(err){
+        console.error('Não foi possível consultar pelo local: ', err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Search/>
+      <div>
+        <input placeholder="teste" value={search} onChange={e => setSearch(e.target.value)}/>
+        <button onClick={ e => searchLocationKey(e)}>Pesquisar</button>
+      </div>
+      {city.WeatherText && (
+        <div>
+          <h2>Condição Atual</h2>
+          <p>Weather: {city.WeatherText}</p>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+const SearchBar = styled.input`
+    width: 90%;
+    height: 2.5em;
+    padding: 0.3em 1em;
+    box-sizing: border-box;
+    border-radius: 12px 0 0 12px;
+    border: none;
+`;
+
+const Container = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 30px;
+    box-sizing: border-box;
+    height: 8em;
+    background-color: #AAD7D9;
+`;
+
+const CustomButton = styled.button`
+    width: 6em;
+    box-sizing: content-box;
+    height: 2.4em;
+    border: none;
+    cursor:pointer;
+    border-radius: 0 12px 12px;
+`
+
+const Search = () => {
+    return(
+        <Container>
+            <SearchBar type="text" placeholder="Digite a cidade, estado, país"/>
+            <CustomButton>Pesquisar</CustomButton>
+        </Container>
+    )
+}
+
+export default Search

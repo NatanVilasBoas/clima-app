22128dec59d7df8ce658405281ef8547ad9a37fc
Natan200-2 <natanboas21@outlook.com>
2024-02-02 17:03:05 -0300

    consulta aos detalhes

--- src/pages/Detalhes.jsx
+++ src/pages/Detalhes.jsx
@@ -31,7 +31,21 @@ const Detalhes = () => {
             {cityDetails ? (
                 <div>
                     {cityDetails.RealFeelTemperature && cityDetails.RealFeelTemperature.Metric ? (
-                        <p>{cityDetails.RealFeelTemperature.Metric.Phrase}</p>
+                        <div>
+                            <h3>Sensação Térmica:</h3>
+                            <p>{`${cityDetails.RealFeelTemperature.Metric.Value}°C`}</p>
+                            <p>{cityDetails.RealFeelTemperature.Metric.Phrase}</p>
+                            <h3>Humidade Relativa:</h3>
+                            <p>{cityDetails.RelativeHumidity}%</p>
+                            <h3>Vento:</h3>
+                            <p>{cityDetails.Wind.Direction.Degrees}° {cityDetails.Wind.Direction.Localized}</p>
+                            <h5>Velocidade:</h5>
+                            <p>{cityDetails.Wind.Speed.Metric.Value}km/h</p>
+                            <h3>Máxima:</h3>
+                            <p>{cityDetails.TemperatureSummary.Past12HourRange.Maximum.Metric.Value}°C</p>
+                            <h3>Mínima:</h3>
+                            <p>{cityDetails.TemperatureSummary.Past12HourRange.Minimum.Metric.Value}°C</p>
+                        </div>
                     ) : (
                         <p>Falha ao obter os detalhes da temperatura</p>
                     )}

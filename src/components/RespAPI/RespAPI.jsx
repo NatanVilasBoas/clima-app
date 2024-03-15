760988c8e3e8bc75d5366bf021f358c4392c3a8a
Natan200-2 <natanboas21@outlook.com>
2024-03-15 15:29:29 -0300

    🎨 🔥 removendo e ajustando componentes e seus estilos

--- src/components/RespAPI/RespAPI.jsx
+++ src/components/RespAPI/RespAPI.jsx
@@ -5,6 +5,8 @@ import wheaterService from "../../services/wheater";
 
 const Context = styled.section`
     display: flex;
+    align-items: center;
+    justify-content: center;
     background: transparent;
     transition: background 0.5s ease;
 `
@@ -23,15 +25,6 @@ const ClimaText = styled.p`
   color: white;
 `
 
-const GridContainer = styled.div`
-    margin: 3vw;
-    display: grid;
-    text-align: center;
-    grid-template-rows: repeat(2, 1fr);
-    grid-template-columns: repeat(2, 1fr);
-    gap: 8px;
-`
-
 const Card = styled.div`
     border: 1px solid #E5E1DA;
     padding: 1em;
@@ -39,16 +32,10 @@ const Card = styled.div`
     background-color: #FBF9F1;
     text-align: center;
     box-sizing: border-box;
-    width: 100%;
-    height: 100%;
+    margin: 2em;
+    width: 80%;
+    height: 40%;
     box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1)
-`
-
-const CardWind = styled(Card)`
-    grid-row: span 2;
-    grid-column: span 2;
-    display: flex;
-    justify-content: space-around;
 `
 
 const RespAPI = ({ search }) => {
@@ -81,11 +68,9 @@ const RespAPI = ({ search }) => {
             <Context>
                 {loading ? <Loader /> :
                     Object.keys(clima).length > 0 ? (
-                        <Content>
-                            <p style={{ fontSize: '18px', fontWeight: '600', margin: '0' }}>Clima Atual em {search}</p>
-                            <ClimaText>{clima[0].WeatherText}</ClimaText>
-                            <GridContainer>
-                                <CardWind>
+                                <Card>
+                                    <p style={{ fontSize: '18px', fontWeight: '600', margin: '0' }}>Clima Atual em {search}</p>
+                                    <p>{clima[0].WeatherText}</p>
                                     <div>
                                         <h3>Máxima:</h3>
                                         <p>{clima[0].TemperatureSummary.Past12HourRange.Maximum.Metric.Value}°C</p>
@@ -94,17 +79,11 @@ const RespAPI = ({ search }) => {
                                         <h3>Mínima:</h3>
                                         <p>{clima[0].TemperatureSummary.Past12HourRange.Minimum.Metric.Value}°C</p>
                                     </div>
-                                </CardWind>
-                                <Card>
                                     <h3>Sensação Térmica:</h3>
                                     <p>{`${clima[0].RealFeelTemperature.Metric.Value}°C`}</p>
                                     <p>{clima[0].RealFeelTemperature.Metric.Phrase}</p>
-                                </Card>
-                                <Card>
                                     <h3>Humidade Relativa:</h3>
                                     <p>{clima[0].RelativeHumidity}%</p>
-                                </Card>
-                                <CardWind>
                                     <div>
                                         <h3>Vento:</h3>
                                         <p>{clima[0].Wind.Direction.Degrees}° {clima[0].Wind.Direction.Localized}</p>
@@ -113,9 +92,7 @@ const RespAPI = ({ search }) => {
                                         <h3>Velocidade:</h3>
                                         <p>{clima[0].Wind.Speed.Metric.Value}km/h</p>
                                     </div>
-                                </CardWind>
-                            </GridContainer>
-                        </Content>
+                                </Card>
                     ) : (<Content>
                         <h2>Seja bem-Vindo</h2>
                         <ClimaText>Pesquise por sua cidade</ClimaText>

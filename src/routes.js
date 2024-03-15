62bf49142c3e052a4ea2152c6fb158f6bc4f702b
Natan200-2 <natanboas21@outlook.com>
2024-03-15 15:04:11 -0300

    🔥 removendo pagina de detalhes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicial from './pages/Inicial';
 import PaginaBase from './pages/PaginaBase';
-import Detalhes from './pages/Detalhes';
 
 function AppRoutes() {
     return (
@@ -9,7 +8,6 @@ function AppRoutes() {
             <Routes>
                 <Route path='/' element={<PaginaBase />}>
                     <Route index element={<Inicial />}></Route>
-                    <Route path='/:id' element={<Detalhes />}></Route>
                 </Route>
             </Routes>
         </BrowserRouter>

16535c6a020a1b3e93889cc57ae555d554b13c0b
Natan200-2 <natanboas21@outlook.com>
2024-03-25 16:01:15 -0300

    🔥 removendo temas

--- src/pages/PaginaBase.jsx
+++ src/pages/PaginaBase.jsx
@@ -1,34 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Tema from "../tema/Tema";
 import Footer from "../components/Footer/Footer";
-import { useClimaContext } from "../context/Clima";
-import styled from "styled-components";
-
-const Container = styled.div`
-    background: ${props => {
-        const temperatura = props.$temperatura;
-
-        if (temperatura < 15) {
-            return props.theme.cold;
-        } else if (temperatura < 25) {
-            return props.theme.warm;
-        } else {
-            return props.theme.hot;
-        }
-    }};
-`
-
 const PaginaBase = () => {
 
-    const { temperatura } = useClimaContext();
-
     return (
-        <Tema>
-            <Container $temperatura={temperatura} >
-                <Outlet />
-                <Footer />
-            </Container>
-        </Tema>
+        <>
+            <Outlet />
+            <Footer />
+        </>
     )
 }
 

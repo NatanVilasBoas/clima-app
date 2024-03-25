0e1199f48a4eeca00dd471005a8fbba68a9b8c84
Natan200-2 <natanboas21@outlook.com>
2024-03-25 16:08:11 -0300

    🚑 🎨 ajuste em cor do background

--- src/pages/PaginaBase.jsx
+++ src/pages/PaginaBase.jsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
+import styled from "styled-components";
+
+const Container = styled.div`
+    background: linear-gradient(0deg, rgba(169,201,201,1) 0%, rgba(154,211,224,1) 100%);
+`
+
 const PaginaBase = () => {
 
     return (
-        <>
+        <Container >
             <Outlet />
             <Footer />
-        </>
+        </Container>
     )
 }
 
